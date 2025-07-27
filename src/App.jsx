9fdf18efe0682f0ddbdeab1dacb13540f4099c9e
Min-byeonghyun <qudgus5125@naver.com>
2025-07-27 20:24:15 +0900

    project settings

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import Home from "./pages/Home";
+import Diary from "./pages/Diary";
+import New from "./pages/New";
+import Notfound from "./pages/Notfound";
+import { Routes, Route } from "react-router-dom";
+
+// 1. '/' : 모든 익리를 조회하는 HOME
+// 2. '/new : 새로운 일기를 작성하는 New페이지
+// 3. '/diary : 일기 상세페이지
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/new" element={<New/>}/>
+      <Route path="/diary" element={<Diary/>}/>
+      <Route path="*" element={<Notfound/>}/>
+    </Routes>
+  )
+}
+
+export default App;
