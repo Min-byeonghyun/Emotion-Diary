624249c3f714cb3dab4462f63a0342d1fd7ed3e2
Min-byeonghyun <qudgus5125@naver.com>
2025-07-28 00:34:51 +0900

    emotion util fuc create

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
 import Notfound from "./pages/Notfound";
-import { Routes, Route } from "react-router-dom";
 
 // 1. '/' : 모든 일기를 조회하는 HOME
 // 2. '/new : 새로운 일기를 작성하는 New페이지
@@ -12,12 +12,12 @@ import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/new" element={<New/>}/>
-      <Route path="/diary/:id" element={<Diary/>}/>
-      <Route path="*" element={<Notfound/>}/>w
+      <Route path="/" element={<Home />} />
+      <Route path="/new" element={<New />} />
+      <Route path="/diary/:id" element={<Diary />} />
+      <Route path="*" element={<Notfound />} />w
     </Routes>
-  )
+  );
 }
 
 export default App;
