37090bc9f9e858a3c58e63fa3a3e7e6f795617a7
Min-byeonghyun <qudgus5125@naver.com>
2025-07-29 17:29:08 +0900

    diary filter create

--- src/components/DiaryList.jsx
+++ src/components/DiaryList.jsx
@@ -2,13 +2,34 @@ import "./DiaryList.css";
 import Button from "./Button";
 import DiaryItem from "./DiaryItem";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 export default function DiaryList({ data }) {
   const nav = useNavigate();
+  const [sortType, setSortType] = useState("latest");
+
+  const onChangeSortType = (e) => {
+    setSortType(e.target.value);
+  };
+
+  const getSortedDate = () => {
+    return data.toSorted((a, b) => {
+      if (sortType === "oldest") {
+        return Number(a.createdDate) - Number(b.createdDate);
+      } else {
+        return Number(b.createdDate) - Number(a.createdDate);
+      }
+    });
+  };
+  const sortedData = getSortedDate();
+
   return (
     <div className="DiaryList">
       <div className="menu_bar">
-        <select>
-          <option value={"latest"}>최신순</option>
+        <select onChange={onChangeSortType}>
+          <option onChange={onChangeSortType} value={"latest"}>
+            최신순
+          </option>
           <option value={"oldest"}>오래된 순</option>
         </select>
         <Button
@@ -18,7 +39,7 @@ export default function DiaryList({ data }) {
         />
       </div>
       <div className="list_wrapper">
-        {data.map((item) => (
+        {sortedData.map((item) => (
           <DiaryItem key={item.id} {...item} />
         ))}
       </div>
