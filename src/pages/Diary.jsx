fcd90f785ef2e3754074009e434026cdbacb3561
Min-byeonghyun <qudgus5125@naver.com>
2025-07-30 16:49:42 +0900

    custom hooks , util func 분리, diarypage 구현

--- src/pages/Diary.jsx
+++ src/pages/Diary.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import { useNavigate, useParams } from "react-router-dom";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Viewer from "../components/Viewer";
+import useDiary from "../hooks/useDiary";
+import { getStringedDate } from "../util/getStringedDate";
 
 export default function Diary() {
+  const params = useParams();
+  const nav = useNavigate();
+
+  const curDiaryItem = useDiary(params.id);
+
+  if (!curDiaryItem) {
+    return <div>데이터 로딩중...!</div>;
+  }
+  const { createdDate, emotionId, content } = curDiaryItem;
+  const title = getStringedDate(new Date(createdDate));
   return (
-    <div>Diary</div>
-  )
+    <div>
+      <Header
+        title={`${title} 일기`}
+        leftChild={<Button onClick={() => nav(-1)} text={"<뒤로 가기"} />}
+        rightChild={
+          <Button onClick={() => nav(`/edit/${params.id}`)} text={"수정하기"} />
+        }
+      />
+      <Viewer emotionId={emotionId} content={content} />
+    </div>
+  );
 }
